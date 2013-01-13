c1dd549fdfea461baf88cf249695a9e213d2142f
Barrett Lewis <musikal.fusion@gmail.com>
2013-01-12 21:26:25 -0800

    Created Testing script
    Testing script has been created to discover all the tests in our project
    in the unit_test package. It is naive and just runs all tests and
    displays the result. This currently is just run from the cmd line but
    should allow for running any tests we create as long as they are
    1) in the unit_test package
    2) file names match the pattern *_tests.py

---
+++ tester.py
@@ -0,0 +1,21 @@
+'''
+Runs all tests for Subtaneous.
+
+Discovers all the tests that are in the project
+by searching the unit_tests package for any tests.
+
+Test modules MUST follow the pattern *_tests.py
+'''
+
+import unittest
+from os import getcwd, path
+
+
+loader = unittest.TestLoader()
+testSuite = loader.discover(
+                            path.join(getcwd(), 'unit_tests'),
+                            pattern='*_tests.py',
+                            top_level_dir=getcwd(),
+                          )
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(testSuite)
